58a8495a0cbae810f2228ae523405927a9c3e7c8
nikhilm25 <nikhilmaan25@gmail.com>
2025-05-17 21:22:49 +0530

    testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,29 @@
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class Main {
+    
+    HashMap<String, Function<String, Void>> mapOfCommands = new HashMap<>();
+
+
+
+
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
         while (true) { 
             System.out.print("$ ");
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+            String input = scanner.nextLine();
+            input = input.trim();
+            String[] arr = input.split("\\s+", 2);
+
+            
+
+
+            System.out.println(input + ": command not found");
         }
+
+        
     }
 }
