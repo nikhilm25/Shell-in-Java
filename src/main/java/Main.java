800ddade0b8aa89a31224fc318813f18685f3b43
nikhilm25 <nikhilmaan25@gmail.com>
2025-05-21 11:19:27 +0530

    adding custom executable execution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 import java.io.File;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class Main {
     
@@ -28,12 +32,32 @@ public class Main {
 
     public static String[] paths = pathEnv.split(File.pathSeparator);
 
+    public static HashMap<String, Path> externalExecutables = new HashMap<>();
 
+    static {
+        for (String path : paths) {
+            Path properPath = Paths.get(path);
 
-
-
-
-
+            if (Files.isRegularFile(properPath)) {
+                if (Files.isExecutable(properPath)) {
+                    String base = properPath.toFile().getName();
+                    externalExecutables.put(base, properPath);
+                }
+            } else if (Files.isDirectory(properPath)) {
+                try (Stream<Path> paths = Files.list(properPath)) {
+                    paths
+                            .filter(Files::isRegularFile)
+                            .filter(Files::isExecutable)
+                            .forEach(p -> {
+                                String base = p.getFileName().toString();
+                                externalExecutables.put(base, p);
+                            });
+                } catch (IOException e) {
+                    e.printStackTrace(); // Handle or log properly
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -44,14 +68,31 @@ public class Main {
             String[] arr = input.split("\\s+", 2);
             String commandName = arr[0];
 
+
             if (mapOfCommands.containsKey(commandName)) {
                 if (arr.length == 2) {
                     String argument = arr[1];
                     mapOfCommands.get(commandName).apply(argument);
-                    continue;
                 }
+            } else if (externalExecutables.containsKey(commandName)) {
+                List<String> commandList = new ArrayList<>();
+                commandList.add(externalExecutables.get(commandName).toString());
+                if (arr.length >= 2) {
+                    String[] arguments = arr[1].split("\\s+");
+                    for (String argument : arguments) {
+                        if (!argument.isEmpty()) commandList.add(argument);
+                    }
+                }
+                ProcessBuilder pb = new ProcessBuilder(commandList);
+                try {
+                    Process process = pb.inheritIO().start();
+                    process.waitFor();
+                } catch (Exception e) {
+                    System.out.println("Error running external command: " + e.getMessage());
+                }
+            } else {
+                System.out.println(input + ": command not found");
             }
-            System.out.println(input + ": command not found");
         }
 
         
